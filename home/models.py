872aeabe2b9595255e93a350198e7672b67fbfe8
vasicev <vasicevdeki@gmail.com>
2021-04-20 08:49:51 +0200

    Add models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created = models.DateTimeField(default=datetime.now)
+    status = models.BooleanField()
+
+    def __str__(self):
+        return self.title + ' status active: ' + str(self.status)
+
+class Tag(models.Model):
+    title = models.CharField(max_length=200)
