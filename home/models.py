62872b75c0bd32e3763c501a348b0bbcd9cf9a38
vasicev <vasicevdeki@gmail.com>
2021-04-21 07:54:41 +0200

    Change ManyToMany

--- home/models.py
+++ home/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 from datetime import datetime
 
-class Task(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    created = models.DateTimeField(default=datetime.now)
-    status = models.BooleanField()
-
-    def __str__(self):
-        return self.title + ' status active: ' + str(self.status)
-
 class Tag(models.Model):
     title = models.CharField(max_length=200)
-    task = models.ManyToManyField(Task)
+
+    def __str__(self):
+        return self.title
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    tags = models.ManyToManyField(Tag)
+    description = models.TextField()
+    created = models.DateTimeField(default=datetime.now)
+    active = models.BooleanField()
 
     def __str__(self):
         return self.title
